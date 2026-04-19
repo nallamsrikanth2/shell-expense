9cbecb01d0e0f71a55a97ed4d61a9584c48c4de2
venkateshGadiparthi <venkateshchowdary7171@gmail.com>
2026-04-19 11:35:09 +0530

    shell-expense

--- 03-frontend.sh
+++ 03-frontend.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
+set -e
+error_handling(){
+    echo "occured at line number $1: error command:$2"
+}
+trap 'error_handling ${LINENO} "$BASH_COMMAND"' ERR
+
 source ./common.sh
 CHECK_ROOT
 
 dnf install nginx -y   &>>$LOG_FILE
-VALIDATE $? "install nginx"
+#VALIDATE $? "install nginx"
 
 systemctl enable nginx  &>>$LOG_FILE
-VALIDATE $? "enable nginx"
+#VALIDATE $? "enable nginx"
 
 systemctl start nginx    &>>$LOG_FILE
-VALIDATE $? "start nginx"
+#VALIDATE $? "start nginx"
 
 rm -rf /usr/share/nginx/html/*  &>>$LOG_FILE
-VALIDATE $? "remove in everything in html"
+#VALIDATE $? "remove in everything in html"
 
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
-VALIDATE $? "download the code"
+#VALIDATE $? "download the code"
 
 cd /usr/share/nginx/html  &>>$LOG_FILE
-VALIDATE $? "move to directory"
+#VALIDATE $? "move to directory"
 
 unzip /tmp/frontend.zip  &>>$LOG_FILE
-VALIDATE $? "unzip the frontend code"
+#VALIDATE $? "unzip the frontend code"
 
 cp /home/ec2-user/shell-expense/expense.conf /etc/nginx/default.d/expense.conf  &>>$LOG_FILE
-VALIDATE $? "copy the code"
+#VALIDATE $? "copy the code"
 
 systemctl restart nginx  &>>LOG_FILE
-VALIDATE $? "restart the nginx"
+#VALIDATE $? "restart the nginx"
 
 echo -e "$G frontend server is created successfully $N"  &>>LOG_FILE
