f1b8c2e28d8b3a6ed7435d51d43c22ebd8c5bd0c
venkateshGadiparthi <venkateshchowdary7171@gmail.com>
2026-04-19 10:37:44 +0530

    shell-expense

--- 02-backend.sh
+++ 02-backend.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$( echo $o | cut -d "." -f1)
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
 LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 R="\e[31m"
