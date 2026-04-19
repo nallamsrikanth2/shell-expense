60364d6f0e7abd69daa5970740f046c48fc604af
venkateshGadiparthi <venkateshchowdary7171@gmail.com>
2026-04-19 11:20:03 +0530

    shell-expense

--- 01-db.sh
+++ 01-db.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
+set -e
+error_handling(){
+    echo "occured at line number $1: error command:$2"
+}
+
 source ./common.sh
 CHECK_ROOT
 
-dnf install mysql-server -y  &>>$LOG_FILE
-VALIDATE $? "install the mysql server"
+dnf install mysql-serverr -y  &>>$LOG_FILE
+#VALIDATE $? "install the mysql server"
 
 systemctl enable mysqld   &>>$LOG_FILE
-VALIDATE $? "enable mysqld"
+#VALIDATE $? "enable mysqld"
 
 systemctl start mysqld     &>>$LOG_FILE
-VALIDATE $? "start the mysqld"
+#VALIDATE $? "start the mysqld"
 
 mysql -h db.nsrikanth.online -uroot -pExpenseApp@1 -e 'show databases;'   &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ExpenseApp@1
-    VALIDATE $? "setup the root password"
+    #VALIDATE $? "setup the root password"
 else
     echo -e "root password alredy setup ......... $Y skipping $N"
 fi
--- common.sh
+++ common.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+trap 'error_handling ${LINENO} "$BASH-COMMAND"' ERR
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
