b398328aff51f9ab244401f5a8d8200671e8919e
venkateshGadiparthi <venkateshchowdary7171@gmail.com>
2026-04-19 11:28:50 +0530

    shell-expense

--- 02-backend.sh
+++ 02-backend.sh
@@ -1,61 +1,67 @@
 #!/bin/bash
+set -e
+error_handling(){
+    echo "occured at line number $1: error command:$2"
+}
+trap 'error_handling ${LINENO} "$BASH_COMMAND"' ERR
+
 source ./common.sh
 CHECK_ROOT
 
 dnf module disable nodejs -y  &>>$LOG_FILE
-VALIDATE $? "disable nodejs"
+#VALIDATE $? "disable nodejs"
 
 dnf module enable nodejs:20 -y  &>>$LOG_FILE
-VALIDATE $? "enable nodejs"
+#VALIDATE $? "enable nodejs"
 
 dnf install nodejs -y   &>>$LOG_FILE
-VALIDATE $? "install nodejs"
+#VALIDATE $? "install nodejs"
 
 id expense
 if [ $? -ne 0 ]
 then
     useradd expense
-    VALIDATE $? "create user"
+    #VALIDATE $? "create user"
 else
     echo -e "user already created .... $Y Skipping $N"
 fi
 
 mkdir -p /app  &>>$LOG_FILE
-VALIDATE $? "create the app directory"
+#VALIDATE $? "create the app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip   &>>$LOG_FILE
-VALIDATE $? "download the backend code"
+#VALIDATE $? "download the backend code"
 
 cd /app  &>>$LOG_FILE
-VALIDATE $? "move to app directory"
+#VALIDATE $? "move to app directory"
 
 rm -rf /app/*  &>>$LOG_FILE
-VALIDATE $? "remove the files in app directory"
+#VALIDATE $? "remove the files in app directory"
 
 unzip /tmp/backend.zip  &>>$LOG_FILE
-VALIDATE $? "unzip the backend code"
+#VALIDATE $? "unzip the backend code"
 
 cd /app  &>>$LOG_FILE
 
 npm install  &>>$LOG_FILE
-VALIDATE $? "install the libries and dependies"
+#VALIDATE $? "install the libries and dependies"
 
 cp /home/ec2-user/expense/backend.service   /etc/systemd/system/backend.service  &>>$LOG_FILE
 
 systemctl daemon-reload   &>>$LOG_FILE
-VALIDATE $? "realod the code"
+#VALIDATE $? "realod the code"
 
 systemctl start backend   &>>$LOG_FILE
-VALIDATE $? "start the backend"
+#VALIDATE $? "start the backend"
 
 systemctl enable backend   &>>$LOG_FILE
-VALIDATE $? "enable the code"
+#VALIDATE $? "enable the code"
 
 dnf install mysql -y  &>>$LOG_FILE
-VALIDATE $? "install mysql"
+#VALIDATE $? "install mysql"
 
 mysql -h db.nsrikanth.online -uroot -pExpenseApp@1 < /app/schema/backend.sql  &>>$LOG_FILE
-VALIDATE $? "load the mysql schema"
+#VALIDATE $? "load the mysql schema"
 
 systemctl restart backend   &>>$LOG_FILE
-VALIDATE $? "restart backend server"
+#VALIDATE $? "restart backend server"
