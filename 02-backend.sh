99c221e3244f5acacd2ebe8525d50e3bbbc6689d
venkateshGadiparthi <venkateshchowdary7171@gmail.com>
2026-04-19 10:42:13 +0530

    shell-expense

--- 02-backend.sh
+++ 02-backend.sh
@@ -28,13 +28,13 @@ else
     echo -e "$G you are in root user $N"
 fi
 
-dnf module disable nodejs -y  &>>$LOGFILE
+dnf module disable nodejs -y  &>>$LOG_FILE
 VALIDATE $? "disable nodejs"
 
-dnf module enable nodejs:20 -y  &>>$LOGFILE
+dnf module enable nodejs:20 -y  &>>$LOG_FILE
 VALIDATE $? "enable nodejs"
 
-dnf install nodejs -y   &>>$LOGFILE
+dnf install nodejs -y   &>>$LOG_FILE
 VALIDATE $? "install nodejs"
 
 id expense
@@ -46,42 +46,42 @@ else
     echo -e "user already created .... $Y Skipping $N"
 fi
 
-mkdir -p /app  &>>$LOGFILE
+mkdir -p /app  &>>$LOG_FILE
 VALIDATE $? "create the app directory"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip   &>>$LOGFILE
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip   &>>$LOG_FILE
 VALIDATE $? "download the backend code"
 
-cd /app  &>>$LOGFILE
+cd /app  &>>$LOG_FILE
 VALIDATE $? "move to app directory"
 
-rm -rf /app/*  &>>$LOGFILE
+rm -rf /app/*  &>>$LOG_FILE
 VALIDATE $? "remove the files in app directory"
 
-unzip /tmp/backend.zip  &>>$LOGFILE
+unzip /tmp/backend.zip  &>>$LOG_FILE
 VALIDATE $? "unzip the backend code"
 
-cd /app  &>>$LOGFILE
+cd /app  &>>$LOG_FILE
 
-npm install  &>>$LOGFILE
+npm install  &>>$LOG_FILE
 VALIDATE $? "install the libries and dependies"
 
 cp /home/ec2-user/expense/backend.service   /etc/systemd/system/backend.service  &>>$LOGFILE
 
-systemctl daemon-reload   &>>$LOGFILE
+systemctl daemon-reload   &>>$LOG_FILE
 VALIDATE $? "realod the code"
 
-systemctl start backend   &>>$LOGFILE
+systemctl start backend   &>>$LOG_FILE
 VALIDATE $? "start the backend"
 
-systemctl enable backend   &>>$LOGFILE
+systemctl enable backend   &>>$LOG_FILE
 VALIDATE $? "enable the code"
 
-dnf install mysql -y  &>>$LOGFILE
+dnf install mysql -y  &>>$LOG_FILE
 VALIDATE $? "install mysql"
 
-mysql -h db.nsrikanth.online -uroot -pExpenseApp@1 < /app/schema/backend.sql  &>>$LOGFILE
+mysql -h db.nsrikanth.online -uroot -pExpenseApp@1 < /app/schema/backend.sql  &>>$LOG_FILE
 VALIDATE $? "load the mysql schema"
 
-systemctl restart backend   &>>$LOGFILE
+systemctl restart backend   &>>$LOG_FILE
 VALIDATE $? "restart backend server"
